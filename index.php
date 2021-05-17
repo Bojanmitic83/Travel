c4e43855a218318d2ff5fddbc58a2caf250b66bb
Bojanmitic83 <bojanmitic83@gmail.com>
2021-05-17 11:48:22 +0200

    responsive

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php include "header.php"; ?>
+    
+    <header class="headerIndex">
+        <h1>Vaše putovanje iz snova</h1>
+        <p>Izaberite svoju avanturu</p>
+        <button>Pretražite</button>
+    </header>
+    <!-- destinacije -->
+    <section class="destinacije container">
+        <h2>Najpopularnije destinacije</h2>
+        <p>Rezervisite do kraja meseca za 30% popusta</p>
+        <article class="wrapper">
+            <div>
+                <img src="img/grcka.jpg" alt="">
+                <h5><i class="fas fa-map-marker-alt"></i>Grcka</h5>
+            </div>
+            <div>
+                <img src="img/italija.jpg" alt="">
+                <h5><i class="fas fa-map-marker-alt"></i>Italija</h5>
+            </div>
+            <div>
+                <img src="img/kanada.jpg" alt="">
+                <h5><i class="fas fa-map-marker-alt"></i>Kanada</h5>
+            </div>
+            <div>
+                <img src="img/norveska.jpg" alt="">
+                <h5><i class="fas fa-map-marker-alt"></i>Norveska</h5>
+            </div>
+            <div>
+                <img src="img/rusija.jpg" alt="">
+                <h5><i class="fas fa-map-marker-alt"></i>Rusija</h5>
+            </div>
+            <div>
+                <img src="img/spanija.jpg" alt="">
+                <h5><i class="fas fa-map-marker-alt"></i>Spanija</h5>
+            </div>
+        </article>
+    </section>
+
+    
+<?php include "footer.php"; ?>
+    
+    
---
+++ ponuda.php
@@ -0,0 +1,41 @@
+<?php include "header.php"; ?>
+    
+    <header class="headerPonuda">
+        <div class="container">
+            <h2>Popust 10-30%</h2>
+            <p>Uplata do januara 2021.</p>
+            <button>Pretražite</button>
+        </div>
+    </header>
+    <!-- promotion -->
+    <main>
+    <h2>Iskoristite promotivne pakete</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ex delectus eius perspiciatis iure hic excepturi exercitationem a facilis quis.</p>
+    <section class="promotion container">
+        <article>
+            <img src="img/ponuda1.jpg" alt="">
+        </article>
+        <article>
+            <div class="row">
+                <div class="img">
+                    <img src="img/ponuda2.jpg" alt="">
+                </div>
+                <div class="text">
+                    <h3>Najbolje cene</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate sit repellendus laudantium facilis provident quam excepturi atque vitae, veritatis adipisci.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="img">
+                    <img src="img/ponuda3.jpg" alt="">
+                </div>
+                <div class="text">
+                    <h3>Najbolje lokacije</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate sit repellendus laudantium facilis provident quam excepturi atque vitae, veritatis adipisci.</p>
+                </div>
+            </div>
+        </article>
+    </section>
+    </main>
+    
+<?php include "footer.php"; ?>
